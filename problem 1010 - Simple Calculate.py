2603df7f185caa30bf5cfac88728e51d34ffb996
Md. Nishat Raihan Khan Nijhum <78140810+mdnrkn@users.noreply.github.com>
2023-02-24 21:22:12 +0600

    Add files via upload

---
+++ problem 1010 - Simple Calculate.py
@@ -0,0 +1,8 @@
+a = (input()).split(" ")
+b = (input()).split(" ")
+
+total_1 = int(a[1]) * float(a[2])
+total_2 = int(b[1]) * float(b[2])
+
+Total = total_1 + total_2
+print(f"VALOR A PAGAR: R$ {Total:.2f}")
