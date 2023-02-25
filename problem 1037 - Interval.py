cc379649c987bea44b8fda205a39f5860bce9045
Md. Nishat Raihan Khan Nijhum <78140810+mdnrkn@users.noreply.github.com>
2023-02-25 16:55:17 +0600

    Add files via upload

---
+++ problem 1037 - Interval.py
@@ -0,0 +1,12 @@
+number = float(input())
+
+if number < 0 or number > 100:
+    print("Fora de intervalo")
+elif number <= 25:
+    print("Intervalo [0,25]")
+elif number <= 50:
+    print("Intervalo (25,50]")
+elif number <= 75:
+    print("Intervalo (50,75]")
+else:
+    print("Intervalo (75,100]")
